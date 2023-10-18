84f40b62d885aedcc6794b290540bb697d95dcad
Antony Mitchell <antony.mitchell@tessella.com>
2023-10-18 15:07:06 +0100

    Add tests for markdown table  generation

--- libraries_io_scraper/results_table/make_results_table.py
+++
@@ -1,7 +0,0 @@
-import jinja2
-
-from libraries_io_scraper.models import Dependency
-
-environment = jinja2.Environment(loader=jinja2.FileSystemLoader("./"))
-template = environment.get_template("markdown_table_template.j2")
-print(template.render(dependencies=[Dependency(name="test", version = "123")]))
---
+++ tests/make_results_table/test_make_results_table.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+from pathlib import Path
+from unittest.mock import patch, mock_open
+from libraries_io_scraper.models import Dependency
+from make_results_table import populate_jinja_template, write_template_to_file
+
+class TestMakeResultsTable:
+
+    def test_it_renders_a_valid_markdown_table(self):
+
+        want = """
+| Name  | Version | Rating |
+|-------|----|------|
+| test | 123 |None |
+| test2 | 567 |None |
+"""
+
+
+        test_dependencies = [Dependency(name="test", version = "123"), Dependency(name="test2", version = "567")]
+        got = populate_jinja_template(test_dependencies)
+        assert got == want
+
+    @patch('builtins.open', new_callable=mock_open())
+    def test_writes_the_output_to_a_markdown_file(self, mock_open_file):
+
+            write_template_to_file("")
+
+            mock_open_file.assert_called_once_with('dependency_ratings.md', 'w')
+            mock_open_file.return_value.__enter__().write.assert_called_once_with('')
