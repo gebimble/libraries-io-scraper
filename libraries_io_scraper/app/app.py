7b20f2dc7fc2f6b04a93b52a7bb57918a48fa6eb
Joe Beaver <joseph.beaver@gmail.com>
2023-10-20 22:29:31 +0100

    Wrote the cli app
    Run as to see help description:
    
    ```
    python -m libaries_io_scraper -h
    ```

---
+++ libraries_io_scraper/app/app.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from typing import Callable
+
+import click
+
+from libraries_io_scraper.dependency_operations import node, python
+from libraries_io_scraper.make_results_table import make_results_table
+
+
+@click.group()
+def libioscrape():
+    return None
+
+
+def dependencies_to_markdown_report(
+    dependency_file: Path,
+    output: str,
+    parser: Callable
+) -> None:
+    dependencies = parser(dependencies=dependency_file)
+
+    for deps in dependencies.values():
+        for dep in deps:
+            dep.get_sourcerank("pypi")
+
+    make_results_table(dependencies, output=output)
+
+    return None
+
+
+@libioscrape.command()
+@click.argument("dependency_file")
+@click.option('-o', '--output')
+def py(dependency_file: Path, output: str = "./dependencies.md"):
+    dependencies_to_markdown_report(
+        dependency_file, output, python.parse_dependencies_file)
+    return None
+
+
+@libioscrape.command()
+@click.argument("dependency_file")
+@click.option('-o', '--output')
+def npm(dependency_file: Path, output: str = "./dependencies.md"):
+    dependencies_to_markdown_report(
+        dependency_file, output, node.parse_dependencies_file)
+    return None
